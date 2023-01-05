4d5706fa9bbf66ed72454238e8c97df09598cd77
Dylan George Field <13308154+dylan-george-field@users.noreply.github.com>
2023-01-05 21:34:05 +1100

    Add instruction to put it in the windows startup folder

--- site/src/App.js
+++ site/src/App.js
@@ -15,8 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Download
+         Download
         </a>
+        <p>Put it in the windows startup folder.</p>
       </header>
     </div>
   );
