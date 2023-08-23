7d38488fb17e32c73a7dffb157967c8f0e54afc4
Dylan George Field <dylan.george.field@gmail.com>
2023-08-23 21:03:16 +1000

    chore (web): fix styling issues on mobile

--- site/src/App.js
+++ site/src/App.js
@@ -5,51 +5,37 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>work from home log</h1>
-        <p className="No-margin-top">Automatically record your WiFi network hourly</p>
+        <p className="no-margin-top">Automatically record your WiFi network hourly</p>
         <img 
            alt="A scrolling animation of the work from home log using microsoft notepad."
            src={require("./wfh-log-demo.gif")}
            className="App-logo"
         />
-          <blockquote>
+        <div className="flex-container">
             <a 
-                className="App-link"
-                href="https://www.ato.gov.au/Media-centre/Media-releases/ATO-announces-changes-to-working-from-home-deductions/"
-                target="_blank"
-                rel="noreferrer"
-            >
-            "Taxpayers need to keep a record of all the hours worked from home for the entire income year – the Australian Tax Office (ATO) won’t accept estimates"
+              className="App-link"
+              href="https://github.com/dylan-george-field/work-from-home-logger/releases/latest/download/wfh-log.exe"
+              target="_blank"
+              rel="noopener noreferrer"
+              id="download">
+                Download (Windows 🪟)
             </a>
-          </blockquote>
-          
-          <br />
-        <p>
-        <span>
-          <a 
+            <span className="divider">|</span>
+            <a
             className="App-link"
-            href="https://github.com/dylan-george-field/work-from-home-logger/releases/latest/download/wfh-log.exe"
+            href="https://github.com/dylan-george-field/work-from-home-log"
             target="_blank"
-            rel="noopener noreferrer"
-            id="download">
-              Download (Windows 🪟)
-          </a>
-           | 
-          <a
-           className="App-link"
-           href="https://github.com/dylan-george-field/work-from-home-logger"
-           target="_blank"
-           rel="noopener noreferrer">
-            Source Code (GitHub)
-          </a>
-        </span>
-        </p>
+            rel="noopener noreferrer">
+              Source Code (GitHub)
+            </a>
+        </div>
         <h2>Installation instructions ✅</h2>
-        <p className="No-margin-top">
-          Download the app <br />
-          Press 'Windows + R' <br />
-          Type 'shell:startup' and hit OK <br />
-          Paste the 'wfh-log.exe' into this folder <br />
-        </p>
+          <ol className="no-margin-top no-list">
+            <li>Download the app</li>
+            <li>Press 'Windows + R'</li>
+            <li>Type 'shell:startup' and hit OK</li>
+            <li>Paste the 'wfh-log.exe' into this folder</li>
+          </ol>
       </header>
       <footer>
         <a href="https://www.georgefield.com.au/contact" rel="noreferrer" target="_blank">Send feedback</a>
